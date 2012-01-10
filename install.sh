4437392275081d316b1e319551e9a14c7fa75417
Alexander Hiam <ahiam@marlboro.edu>
2012-01-10 18:16:55 -0500

    added uninstall option

--- install.sh
+++ install.sh
@@ -1,42 +1,78 @@
 #!/bin/sh
 # Install script for PyBBIO
-# May require root priveledges. 
-
+# May require root priveledges.
 
 PYTHON_PATH="/usr/lib/python2.7"
 BBIO="bbio.py"
-CONFIG_DIR="$HOME/.pybbio/"
+CONFIG_DIR="$HOME/.pybbio"
 CONFIG_FILE="bbio.cfg"
-CONFIG_FILE_DIR="config/"
+CONFIG_FILE_DIR="config"
 
-if [ -h "$PYTHON_PATH/$BBIO" ]; then
-  echo "Updating PyBBIO..."
-  rm "$PYTHON_PATH/$BBIO"
-else 
-  echo "Installing PyBBIO..."
+help() 
+{
+cat <<EndOfHelp
+PyBBIO Installer
+ To install/update: sh install.sh -i
+ To uninstall: sh install.sh -u
+ Display this message: sh install.sh -h
+EndOfHelp
+exit 0
+}
+
+if [ $# -eq 0 ] || [ $1 = "-h" ]; then
+  help
 fi
-cp $BBIO "$PYTHON_PATH/$BBIO"
 
-if [ ! -d "$CONFIG_DIR" ]; then
-  echo "Config directory not found..."
-  mkdir $CONFIG_DIR
-  echo "Created config directory: $CONFIG_DIR"
-else 
-  echo "Config directory found..."
-fi
-
-if [ -f "$CONFIG_DIR/$CONFIG_FILE" ]; then
-  if ! diff "$CONFIG_DIR/$CONFIG_FILE" "$CONFIG_FILE_DIR/$CONFIG_FILE" > /dev/null; 
-  then 
-    echo "Backing up old config file..."
-    if [ ! -d "$CONFIG_DIR/config.old/" ]; then
-      mkdir "$CONFIG_DIR/config.old/"
-    fi
-    STAMP=$(date +"%F_%H-%M")
-    BACKUP="$STAMP-$CONFIG_FILE"
-    mv "$CONFIG_DIR$CONFIG_FILE" "$CONFIG_DIR/config.old/$BACKUP"
+if [ $1 = "-u" ]; then
+  echo "Uninstalling... PyBBIO"
+  if [ -f "$PYTHON_PATH/$BBIO" ]; then
+    rm $PYTHON_PATH/$BBIO
   fi
-fi 
-echo "Copying new config file..."
-cp "$CONFIG_FILE_DIR/$CONFIG_FILE" $CONFIG_DIR
-echo "PyBBIO installed successfully"
+  if [ -d "$CONFIG_DIR" ]; then
+    if [ -d "$CONFIG_DIR/config.old/" ]; then    
+      rm $CONFIG_DIR/config.old/*.*
+      rmdir $CONFIG_DIR/config.old/
+    fi
+    rm $CONFIG_DIR/*.*
+    rmdir $CONFIG_DIR
+  fi
+  echo "PyBBIO uninstalled successfully"
+  exit 0
+fi
+
+if [ $1 = "-i" ]; then
+  if [ -f "$PYTHON_PATH/$BBIO" ]; then 
+    echo "Updating PyBBIO..."
+    rm $PYTHON_PATH/$BBIO
+  else 
+    echo "Installing PyBBIO..."
+  fi
+  cp $BBIO $PYTHON_PATH/$BBIO
+
+  if [ ! -d "$CONFIG_DIR" ]; then
+    echo "Config directory not found..."
+    mkdir $CONFIG_DIR
+    echo "Created config directory: $CONFIG_DIR"
+  else 
+    echo "Config directory found..."
+  fi
+
+  if [ -f "$CONFIG_DIR/$CONFIG_FILE" ]; then
+    if ! diff $CONFIG_DIR/$CONFIG_FILE $CONFIG_FILE_DIR/$CONFIG_FILE > /dev/null; 
+    then 
+      echo "Backing up old config file..."
+      if [ ! -d "$CONFIG_DIR/config.old/" ]; then
+        mkdir $CONFIG_DIR/config.old/
+      fi
+      STAMP=$(date +"%F_%H-%M")
+      BACKUP="$STAMP-$CONFIG_FILE"
+      mv $CONFIG_DIR/$CONFIG_FILE $CONFIG_DIR/config.old/$BACKUP
+    fi
+  fi 
+  echo "Copying new config file..."
+  cp $CONFIG_FILE_DIR/$CONFIG_FILE $CONFIG_DIR
+  echo "PyBBIO installed successfully"
+  exit 0
+fi
+
+help
