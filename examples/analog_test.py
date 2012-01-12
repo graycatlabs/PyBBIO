c5c0a37e7d6f4c25c68b6464fb1a2d2755e87333
Alexander Hiam <ahiam@marlboro.edu>
2012-01-12 12:24:38 -0500

    first commit

---
+++ examples/analog_test.py
@@ -0,0 +1,16 @@
+# analog_test.py - Alexander Hiam
+# Testing analogRead()1
+
+import time
+
+# Must 'import *' to get all the needed constants,
+# simply importing bbio won't do:
+from bbio import *
+
+# Create a BeagleBoard instance:
+bb = BeagleBone()
+
+for i in range(20):
+  print bb.analogRead(AIN7)
+  time.sleep(0.5)
+
