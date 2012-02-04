6e030f8df13e2f6f17f12ad60d76abf090e3cd11
Alexander Hiam <ahiam@marlboro.edu>
2012-02-04 19:55:26 +0000

    made compatible with v0.2

--- examples/analog_test.py
+++ examples/analog_test.py
@@ -1,18 +1,17 @@
 # analog_test.py - Alexander Hiam
-# Testing analogRead()1
+# Testing analogRead()
 
-import time
-
-# Must 'import *' to get all the needed constants,
-# simply importing bbio won't do:
+# Import PyBBIO library:
 from bbio import *
 
-# Create a BeagleBoard instance:
-bb = BeagleBone()
-bb._analog_init()
+# Create a setup function:
+def setup():
+  # Nothing to do here
 
-for i in range(20):
-  print bb.analogRead(AIN7)
-  time.sleep(0.5)
+# Create a main function:
+def main():
+  print analogRead(AIN7)
+  sleep(0.5)
 
-bb.cleanup()
+# Start the loop:
+run(setup, main)
