8d768d2b7da9369d4b76d32060b6c8d58e93ac2d
Alexander Hiam <ahiam@marlboro.edu>
2012-02-04 20:03:26 +0000

    fixed bug

--- examples/analog_test.py
+++ examples/analog_test.py
@@ -7,10 +7,11 @@ from bbio import *
 # Create a setup function:
 def setup():
   # Nothing to do here
+  pass
 
 # Create a main function:
 def main():
-  print analogRead(AIN7)
+  print analogRead('AIN7')
   sleep(0.5)
 
 # Start the loop:
