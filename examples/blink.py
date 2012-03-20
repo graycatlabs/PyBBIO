440b91fa0ac24a621b9527e7cd7b40be496f610b
Alexander Hiam <ahiam@marlboro.edu>
2012-03-20 22:04:10 +0000

    updated to use pin constants instead of strings

--- examples/blink.py
+++ examples/blink.py
@@ -7,18 +7,18 @@ from bbio import *
 # Create a setup function:
 def setup():
   # Set the two LEDs as outputs: 
-  pinMode('USR2', OUTPUT)
-  pinMode('USR3', OUTPUT)
+  pinMode(USR2, OUTPUT)
+  pinMode(USR3, OUTPUT)
 
   # Start one high and one low:
-  digitalWrite('USR2', HIGH)
-  digitalWrite('USR3', LOW)
+  digitalWrite(USR2, HIGH)
+  digitalWrite(USR3, LOW)
 
 # Create a main function:
 def main():
   # Toggle the two LEDs and sleep a few seconds:
-  toggle('USR2')
-  toggle('USR3')
+  toggle(USR2)
+  toggle(USR3)
   sleep(0.5)
 
 # Start the loop:
