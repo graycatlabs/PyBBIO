d383291139d6ee55ca6efe729ea10345ebf7c5b6
Alexander Hiam <ahiam@marlboro.edu>
2012-03-20 22:02:30 +0000

    first commit

---
+++ examples/available_pins.py
@@ -0,0 +1,25 @@
+# available_pins.py - Alexander Hiam
+# Prints all the pins available for IO expansion by their 
+# names used in PyBBIO (refer to beaglebone schematic for
+# header locations).
+
+# Import PyBBIO library:
+from bbio import *
+
+# Create a setup function:
+def setup():
+  print "\n GPIO pins:" 
+  for i in GPIO.keys(): 
+    print "   %s" % i
+  print "\n ADC pins:" 
+  for i in ADC.keys():
+    print "   %s" % i
+
+# Create a main function:
+def main():
+  # No need to keep running
+  stop()
+
+# Start the loop:
+run(setup, main)
+
