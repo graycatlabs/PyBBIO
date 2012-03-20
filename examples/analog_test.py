f50378ff04b06643fd07167c1ee0d1f399a71c47
Alexander Hiam <ahiam@marlboro.edu>
2012-03-20 21:42:31 +0000

    changed input pin

--- examples/analog_test.py
+++ examples/analog_test.py
@@ -4,6 +4,9 @@
 # Import PyBBIO library:
 from bbio import *
 
+# Analog input pin - A0 is pin 39 on header P9 
+ain = A0
+
 # Create a setup function:
 def setup():
   # Nothing to do here
@@ -11,7 +14,7 @@ def setup():
 
 # Create a main function:
 def main():
-  adc_value = analogRead(VSYS)
+  adc_value = analogRead(ain)
   voltage = adc_value * (1.8/2.0**12)
   print "%f v" % voltage
   sleep(0.5)
