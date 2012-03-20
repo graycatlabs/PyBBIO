241d3aaa77973beae005c0fafb1239c0b4d17ac8
Alexander Hiam <ahiam@marlboro.edu>
2012-03-20 21:52:37 +0000

    more comments

--- examples/analog_test.py
+++ examples/analog_test.py
@@ -1,6 +1,11 @@
 # analog_test.py - Alexander Hiam
 # Testing analogRead()
-
+#
+# *** NOTICE *** 
+# The maximum ADC input voltage is 1.8v,
+# applying greater voltages will likely cause
+# permanent damage to the ADC module! 
+ 
 # Import PyBBIO library:
 from bbio import *
 
@@ -15,8 +20,12 @@ def setup():
 # Create a main function:
 def main():
   adc_value = analogRead(ain)
-  voltage = adc_value * (1.8/2.0**12)
-  print "%f v" % voltage
+  # ADC reference voltage is 1.8v, and it is a 
+  # 12-bit ADC, so to get volts per step:
+  volts_per_step = 1.8 / 2.0**12
+  # Multiply ADC value by volts/step to get voltage:
+  voltage = adc_value * volts_per_step
+  print "%fv" % voltage
   sleep(0.5)
 
 # Start the loop:
