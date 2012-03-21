7b033a44f88acbc1fa949ae42616382d3abda260
Alexander Hiam <ahiam@marlboro.edu>
2012-03-21 21:27:51 +0000

    updated to use pin constants instead of strings

--- examples/digitalRead.py
+++ examples/digitalRead.py
@@ -7,13 +7,13 @@ from bbio import *
 # Create a setup function:
 def setup():
   # Set the GPIO pins:
-  pinMode('USR3', OUTPUT)
-  pinMode('GPIO1_6', INPUT)
+  pinMode(USR3, OUTPUT)
+  pinMode(GPIO1_6, INPUT)
 
 # Create a main function:
 def main():
-  state = digitalRead('GPIO1_6')
-  digitalWrite('USR3', state)
+  state = digitalRead(GPIO1_6)
+  digitalWrite(USR3, state)
   # It's good to put a bit of a delay in if possible
   # to keep the processor happy:
   sleep(0.1)
