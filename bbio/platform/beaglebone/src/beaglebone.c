db1831f41452b65f24efae41523c98bbc4f0389d
Alexander Hiam <ahiam@marlboro.edu>
2013-03-19 16:35:57 -0500

    fixed crazy formatting

--- bbio/platform/beaglebone/src/beaglebone.c
+++ bbio/platform/beaglebone/src/beaglebone.c
@@ -15,43 +15,43 @@
 
 static PyObject *cleanup(PyObject *self, PyObject *args) {
   mmapClose();
-	return Py_BuildValue("");
+  return Py_BuildValue("");
 }
 
 static PyObject *getReg(PyObject *self, PyObject *args) {
-	memaddr address;
-	if (!PyArg_ParseTuple(args,"K", &address)) {
-		return NULL;
+  memaddr address;
+  if (!PyArg_ParseTuple(args,"K", &address)) {
+    return NULL;
   }
-	return Py_BuildValue("I", _getReg(address, LITTLEENDIAN, 4));
+  return Py_BuildValue("I", _getReg(address, LITTLEENDIAN, 4));
 }
 
 static PyObject *getReg16(PyObject *self, PyObject *args) {
-	memaddr address;
-	if (!PyArg_ParseTuple(args,"K", &address)) {
-		return NULL;
+  memaddr address;
+  if (!PyArg_ParseTuple(args,"K", &address)) {
+    return NULL;
   }
-	return Py_BuildValue("I", _getReg(address, LITTLEENDIAN, 2));
+  return Py_BuildValue("I", _getReg(address, LITTLEENDIAN, 2));
 }
 
 static PyObject *setReg(PyObject *self, PyObject *args) {
-	memaddr address;
-	uint value;
-	if (!PyArg_ParseTuple(args,"KI", &address, &value)) {
-		return NULL;
+  memaddr address;
+  uint value;
+  if (!PyArg_ParseTuple(args,"KI", &address, &value)) {
+    return NULL;
   }
-	_setReg(address, value, LITTLEENDIAN, 4);
-	return Py_BuildValue("");
+  _setReg(address, value, LITTLEENDIAN, 4);
+  return Py_BuildValue("");
 }
 
 static PyObject *setReg16(PyObject *self, PyObject *args) {
-	memaddr address;
-	uint value;
-	if (!PyArg_ParseTuple(args,"KI", &address, &value)) {
-		return NULL;
+  memaddr address;
+  uint value;
+  if (!PyArg_ParseTuple(args,"KI", &address, &value)) {
+    return NULL;
   }
   _setReg(address, value, LITTLEENDIAN, 2);
-	return Py_BuildValue("");
+  return Py_BuildValue("");
 }
 
 
@@ -59,13 +59,13 @@ static PyMethodDef functions[] = {
   {"test_it", test_it, METH_VARARGS, "testing c extensions"},
   {"cleanup", cleanup, METH_VARARGS, 
 	 "Driver cleanup routine; must be called once before program exit."},
-
-	{"getReg", getReg, METH_VARARGS, "Returns 32 bit value at given register."},
-	{"getReg16", getReg16, METH_VARARGS, "Returns 16 bit value at given register."},
-
-	{"setReg", setReg, METH_VARARGS, "Sets 32 bit register to given value."},
-	{"setReg16", setReg16, METH_VARARGS, "Sets 16 bit register to given value."},
-
+  
+  {"getReg", getReg, METH_VARARGS, "Returns 32 bit value at given register."},
+  {"getReg16", getReg16, METH_VARARGS, "Returns 16 bit value at given register."},
+  
+  {"setReg", setReg, METH_VARARGS, "Sets 32 bit register to given value."},
+  {"setReg16", setReg16, METH_VARARGS, "Sets 16 bit register to given value."},
+  
   { NULL }
 };
 
