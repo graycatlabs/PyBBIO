4a7012352d6fd207565441b87f2fa8894a1abafb
deepakkarki <deepak6k@gmail.com>
2014-03-05 19:04:07 +0530

    added i2c configuration data

--- bbio/platform/beaglebone/3.8/config.py
+++ bbio/platform/beaglebone/3.8/config.py
@@ -122,7 +122,26 @@ UART = {
   'UART5' : ['/dev/ttyO5', 'BB-UART5']
 }
 
-
 ##--- End UART config ------##
+##############################
+
+
+##############################
+##--- Start I2C config: ---##
+
+# I2C bus address must be in form: 
+#    [dev-entry, I2C-overlay-name]
+# rather confusing bus address and dev-entry don't exactly match
+# i2c0, i2c2 buses are activated by default i.e. /dev/12c-0 and /dev/i2c1
+# NOTE : 1st I2C bus - i2c0 is used to read eeproms of capes - Don't use that!!
+# more info - http://datko.net/2013/11/03/bbb_i2c/
+
+I2C = {
+  'i2c0' : ['/dev/i2c-0', 'BB-I2C0'],
+  'i2c1' : ['/dev/i2c-2', 'BB-I2C1'],
+  'i2c2' : ['/dev/i2c-1', 'BB-I2C2'],
+}
+
+##--- End I2C config ------##
 ##############################
 
