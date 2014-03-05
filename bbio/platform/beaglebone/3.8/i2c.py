4a1e660f542a2f95488ab0357c1102d5cab1a33a
deepakkarki <deepak6k@gmail.com>
2014-03-05 21:34:25 +0530

    added basic functionality of open, close, read and write

--- bbio/platform/beaglebone/3.8/i2c.py
+++ bbio/platform/beaglebone/3.8/i2c.py
@@ -11,6 +11,8 @@
 #		to activate i2c1 bus; echo BB-I2C1 > /sys/devices/bone_capemgr.8/slots; will be now present at /dev/i2c-2
 #		reference : http://datko.net/2013/11/03/bbb_i2c/
 
+import config
+
 try:
   import smbus
 except:
@@ -21,16 +23,21 @@ class _I2C_BUS(object):
 	def __init__(self, bus):
 		'''
 		self : _I2C_BUS object
-		bus : string - represents bus address eg. i2c0, i2c1
+		bus : string - represents bus address eg. i2c1, i2c2
 		'''
 		self.config = bus
 		self.bus = None # This is the smbus object
+		self.open = False
 
 	def begin(self):
 		'''
 		Initializes the I2C bus with BBB as master
 		'''
-		pass
+		if not i2cInit(self.config):
+			print "Could not initialize i2c bus : %s" % self.config 
+			return
+		self.bus = smbus.SMBus(int(I2C[self.config][0][-1]))
+		self.open = True
 
 	def write(self, addr, val):
 		'''
@@ -39,22 +46,59 @@ class _I2C_BUS(object):
 		val : string, integer or list - if list, writes each value in the list
 		returns number of bytes written
 		'''
+		if not self.open:
+			print "I2C bus : %s - not initialized" % self.config
+
+		if type(addr) == int:
+			return self.bus.write_byte(addr, val)
+
+		else:
+			return self._write_list(addr, self._process(val))
+
+
+	def _write_list(self, addr, val):
+		'''
+		called from write when val is a string or list - this is internal to keep interface simple
+		addr : integer between (0-127) - Address of slave device
+		val : list - Of either characters or integers.
+		'''
 		pass
+
 
 	def read(self, addr, size=1):
 		'''
 		Reads 'size' number of bytes from slave device 'addr'
 		addr : integer between (0-127) - Address of slave device
 		size : integer - number of bytes to be read
-		returns the bytes read as a tuple
+		returns an int if size is 1; else list of integers
 		'''
-		pass
+		if not self.open:
+			print "I2C bus : %s - not initialized, open before read" % self.config
+
+		if size == 1:
+			return self.bus.read_byte(addr)
+
+		read_data = []
+
+		else:
+			for i in range(size):
+				data = self.bus.read_byte(addr)
+				if data == -1: #No more data to be read
+					break
+				else :
+					read_data.append(data)
+
+		return read_data
+
 
 	def end(self):
 		'''
 		BBB exits the bus
 		'''
-		pass
+		if self.bus:
+			self.bus.close()
+
+
 
 	def _process(self, val):
 		'''
