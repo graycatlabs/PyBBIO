fa4bcd71d725e6114e32405622a0825ff49f2130
Alaa Agwa <agwatic@gmail.com>
2014-03-18 11:00:43 +0200

    fix the example file

--- examples/tmp36-lm35.py
+++ examples/tmp36-lm35.py
@@ -1,10 +1,10 @@
 """
- tmp36-lm35.py 
+ tmp36-lm35.py
  Alaa Agwa - 3/2014
- 
+
  Example program for PyBBIO's TMP36LM35 library.
  Reads the temerature from the TMP36 or LM35 sensors from analog pin.
- 
+
  This example program is in the public domain.
 """
 from bbio import *
@@ -24,5 +24,7 @@ def loop():
     	print "Error reading the sensor value"
   	else:
     	print "Temp: %0.2f C" % temp
-  
-	delay(1000) 
+
+	delay(1000)
+
+run(setup,loop)
