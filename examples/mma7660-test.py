1058273badd733ecc38936c60987c75541b767f7
rseetham <rekha.kmit@gmail.com>
2014-04-23 21:33:04 +0000

    change in orientation does not cause interrupt

--- examples/mma7660-test.py
+++ examples/mma7660-test.py
@@ -6,38 +6,35 @@ INT_PIN = GPIO1_28
 accel = MMA7660(2)
 
 def accelCallback(back_front, portrait_landscape, tap, shake):
-  if portrait_landscape == 1:
-    print "Orientation : Left: Equipment is in landscape mode to the left"
-  elif portrait_landscape == 2:
-    print "Orientation : Right: Equipment is in landscape mode to the right"
-  elif portrait_landscape == 5:
-    print "Orientation : Down: Equipment standing vertically in inverted orientation"
-  elif portrait_landscape == 6:
-    print "Orientation : Up: Equipment standing vertically in normal orientation"
-  if back_front == 1 :
-    print "Orientation : Front: Equipment is lying on its front"
-  elif back_front == 2:
-    print "Orientation : Back: Equipment is lying on its back"
   if tap == 1:
     print "Tap Detected"
   if shake == 1:
     print "Shake Detected" 
 
 def setup():
-  int_cfg = accel.INT_FB  | \
-            accel.INT_PL  | \
-            accel.INT_PD  | \
-            accel.INT_SHX | \
-            accel.INT_SHY | \
-            accel.INT_SHZ
-             
+  int_cfg = accel.INT_PD  | \
+            accel.INT_SHX             
   accel.setInterrupt(int_cfg, INT_PIN, accelCallback)
+  accel.settapthreshold(4)
+  accel.setTapDebounce(31)
 
   
 def loop():
-  x = accel.getX()
-  y = accel.getY()
-  z = accel.getZ()
-  print "X : "+str(x)+" Y : "+str(y)+" Z : "+str(z)
-  delay(30000)
+  xyz = accel.getXYZ()
+  print "X : "+str(xyz[0])+" Y : "+str(xyz[1])+" Z : "+str(xyz[2])
+  orient = accel.getOrientation()
+  if orient[1] == 1:
+    print "Orientation : Left: Equipment is in landscape mode to the left"
+  elif orient[1] == 2:
+    print "Orientation : Right: Equipment is in landscape mode to the right"
+  elif orient[1] == 5:
+    print "Orientation : Down: Equipment standing vertically in inverted orientation"
+  elif orient[1] == 6:
+    print "Orientation : Up: Equipment standing vertically in normal orientation"
+  if orient[0] == 1 :
+    print "Orientation : Front: Equipment is lying on its front"
+  elif orient[0] == 2:
+    print "Orientation : Back: Equipment is lying on its back"
+  delay(3000)
+
 run(setup,loop)
