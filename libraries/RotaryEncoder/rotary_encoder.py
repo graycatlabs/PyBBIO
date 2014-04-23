299451244badcf6b12d80f1dd568b94385c0ca34
rseetham <rekha.kmit@gmail.com>
2014-04-23 20:21:21 +0000

    debugging

--- libraries/RotaryEncoder/rotary_encoder.py
+++ libraries/RotaryEncoder/rotary_encoder.py
@@ -1,6 +1,7 @@
 
-from bbio.sysfs import kernelFilenameIO
-from bbio import * #addToCleanup, cape_manager, config for OCP_PATH
+from bbio.platform import sysfs
+from bbio import addToCleanup, cape_manager
+from bbio import OCP_PATH
 
 class RotaryEncoder(object):
   _eqep_dirs = [
@@ -10,12 +11,12 @@ class RotaryEncoder(object):
   ]
   
     
-  def __init__(self, eqep_num,mode):
-    assert 0 <= eqep < 3, "eqep_num must be between 0 and 2"
-    assert 0 <= mode < 2, "mode must be either 0 (A) or 1(R)"
+  def __init__(self, eqep_num):
+    assert 0 <= eqep_num < 3, "eqep_num must be between 0 and 2"
+ 
     
     try:
-      cape_manager.load('PyBBIO-bone_epeq%s' % eqep_num, not preserve_mode_on_exit)
+      cape_manager.load('PyBBIO-bone_epeq%s' % eqep_num, 1)
       bbio.delay(250) # Give driver time to load
     except IOError:
       print "*Could not load bone_epeq%s overlay, resource busy" % eqep_num
@@ -35,6 +36,20 @@ class RotaryEncoder(object):
   def disable(self):
     enable_file = "%s/enabled" % self.base_dir
     return kernelFilenameIO(enable_file, 0)
+
+  def setAbsolute(self):
+    '''
+    Set mode as Absolute
+    '''
+    set_mode = "%s/mode" % self.base_dir
+    return kernelFilenameIO(enable_file, 0)
+    
+  def setRelative(self):
+    '''
+    Set mode as Relative
+    '''
+    set_mode = "%s/mode" % self.base_dir
+    return kernelFilenameIO(enable_file, 1)
     
   def getMode(self):
     mode_file = "%s/enabled" % self.base_dir
