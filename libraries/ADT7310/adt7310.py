70928798bd43f2155f14ce01d41410da675cc888
rseetham <rekha.kmit@gmail.com>
2014-04-24 05:40:50 +0000

    library for using the SPI based temperature sensor ADT7310

---
+++ libraries/ADT7310/adt7310.py
@@ -0,0 +1,25 @@
+from bbio import *
+
+class ADT7310(object):
+
+  def __init__(self, spi_no, cs):
+    assert 0 <= spi_no < 2, "spi_no must be between 0 or 1"
+    assert 0 <= cs < 2, "cs must be between 0 or 1"
+    if spi_no == 0:
+      self.spidev = SPI0
+    else:
+      self.spidev = SPI1
+    self.cs = cs
+    self.spidev.begin()
+    self.spidev.setDataMode(self.cs,3)
+    
+  def getTemp(self):
+    self.spidev.write(0x54)
+    _t = self.spidev.read(0,2)
+    if ( _t[0] & 128 == 0):
+      temp = (((_t[0]<<8)+_t[1])>>3)/16
+    else:
+      temp = ((((_t[0]<<8)+_t[1])>>3)-4096)/16
+    return temp
+    
+    
