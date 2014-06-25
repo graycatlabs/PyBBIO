1ee49975dba182d06ef00449965c25257df67503
Alexander Hiam <hiamalexander@gmail.com>
2014-06-25 13:25:09 -0400

    typo

--- bbio/platform/beaglebone/api.py
+++ bbio/platform/beaglebone/api.py
@@ -10,12 +10,12 @@ from bbio.platform.platform import detect_platform
 PLATFORM = detect_platform()
 
 if "3.8" in PLATFORM:
-  from bone_3_8.adc improt analog_init, analog_cleanup
+  from bone_3_8.adc import analog_init, analog_cleanup
   from bone_3_8.pwm import pwm_init, pwm_cleanup
   from serial_port import serial_cleanup
   
 elif "3.2" in PLATFORM:
-  from bone_3_2.adc improt analog_init, analog_cleanup
+  from bone_3_2.adc import analog_init, analog_cleanup
   from bone_3_2.pwm import pwm_init, pwm_cleanup
   from serial_port import serial_cleanup
   
