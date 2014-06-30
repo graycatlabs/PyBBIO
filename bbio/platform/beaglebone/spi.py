ce163926233595a3114a452619a9c0b10ef20a62
rseetham <rekha.kmit@gmail.com>
2014-06-30 14:00:02 +0000

    fixed indentations

--- bbio/platform/beaglebone/spi.py
+++ bbio/platform/beaglebone/spi.py
@@ -24,7 +24,8 @@ class SPI_Bus(object):
     '''
     SPIx.begin()
     Must be called at the start to initialise the bus.
-    Connects the BBB to the SPI Bus specified. Initialises both the devices on the bus.
+    Connects the BBB to the SPI Bus specified. 
+    Initialises both the devices on the bus.
     '''
     spi_init(self.spi_bus)
     self.spidev_cs0.open(self.spi_bus+1,  0)
@@ -44,8 +45,9 @@ class SPI_Bus(object):
     '''
     SPIx.read(chip_select, no_of_bytes_to_be_read)
     chip_select must be either 0 or 1 based on the device to read from.
-    Read no_of_bytes_to_be_read bytes from SPI device selected by chip_select
-    Returns the data read from the device as a list whose each element contains 1 byte 
+    Read no_of_bytes_to_be_read bytes from SPI device selected by chip_select.
+    Returns the data read from the device 
+    as a list whose each element contains 1 byte 
     '''
     assert self._running == True, "Must call SPIx.begin() before using read()"
     assert 0 <= cs < 2, "Chip Select must be 0 or 1"
@@ -78,12 +80,14 @@ class SPI_Bus(object):
     '''
     SPIx.transfer(chip_select,[list])
     Performs SPI transaction. 
-    Writes each element in the list to SPI device specified by the chip_select.
+    Writes each element in the list to SPI device given by the chip_select.
     Simultaneously reads from the SPI device specified by the chip_select.
     CS will be held active between blocks.
-    Returns the data read from the device as a list whose each element contains 1 byte 
+    Returns the data read from the device as a list
+    whose each element contains 1 byte.
     '''
-    assert self._running == True, "Must call SPIx.begin() before using transfer()"
+    assert self._running == True, \
+    "Must call SPIx.begin() before using transfer()"
     assert 0 <= cs < 2, "Chip Select must be 0 or 1"
     if type(data) == str:
       data = [ord(c) for c in data]
@@ -103,12 +107,17 @@ class SPI_Bus(object):
     Sets the Mode of the SPI device specified by chip_select.
     SPI mode as two bit pattern of Clock Polarity  and Phase [CPOL|CPHA].
 	It can take a value between 0<= chip_select <=3.
-	Mode 0 : base value of the clock is zero, data are captured on the clock's rising edge and data is propagated on a falling edge (high→low clock transition).
-	Mode 1 : the base value of the clock is zero, data are captured on the clock's falling edge and data is propagated on a rising edge.
-	Mode 2 : the base value of the clock is one, data are captured on clock's falling edge and data is propagated on a rising edge.
-	Mode 3 : the base value of the clock is one, data are captured on clock's rising edge and data is propagated on a falling edge.
+      Mode 0 : base value of the clock is zero, data are captured on the 
+               clock's rising edge and data is propagated on a falling edge.
+      Mode 1 : the base value of the clock is zero, data are captured on the 
+               clock's falling edge and data is propagated on a rising edge.
+      Mode 2 : the base value of the clock is one, data are captured 
+               on clock's falling edge and data is propagated on a rising edge.
+      Mode 3 : the base value of the clock is one, data are captured on 
+               clock's rising edge and data is propagated on a falling edge.
     '''
-    assert self._running == True, "Must call SPIx.begin() before using setDataMode()"
+    assert self._running == True,\
+        "Must call SPIx.begin() before using setDataMode()"
     assert 0 <= cs < 2, "Chip Select must be 0 or 1"
     assert 0 <= mode < 4, "Mode must be between 0 and 3"
     if cs == 0:
@@ -121,13 +130,18 @@ class SPI_Bus(object):
     SPIx.getDataMode(chip_select)
     Gets the Mode of the SPI device specified by chip_select.
     SPI mode has two bit pattern of Clock Polarity  and Phase [CPOL|CPHA].
-	Mode 0 : base value of the clock is zero, data are captured on the clock's rising edge and data is propagated on a falling edge (high→low clock transition).
-	Mode 1 : the base value of the clock is zero, data are captured on the clock's falling edge and data is propagated on a rising edge.
-	Mode 2 : the base value of the clock is one, data are captured on clock's falling edge and data is propagated on a rising edge.
-	Mode 3 : the base value of the clock is one, data are captured on clock's rising edge and data is propagated on a falling edge.
-	Returns the mode of SPI device.
+    Mode 0 : base value of the clock is zero, data are captured on the 
+             clock's rising edge and data is propagated on a falling edge.
+    Mode 1 : the base value of the clock is zero, data are captured on the 
+             clock's falling edge and data is propagated on a rising edge.
+    Mode 2 : the base value of the clock is one, data are captured on 
+             clock's falling edge and data is propagated on a rising edge.
+    Mode 3 : the base value of the clock is one, data are captured on 
+             clock's rising edge and data is propagated on a falling edge.
+    Returns the mode of SPI device.
     '''
-    assert self._running == True, "Must call SPIx.begin() before using getDataMode()"
+    assert self._running == True, \
+    "Must call SPIx.begin() before using getDataMode()"
     assert 0 <= cs < 2, "Chip Select must be 0 or 1"
     if cs == 0:
       return self.spidev_cs0.mode
@@ -137,11 +151,13 @@ class SPI_Bus(object):
   def setBitOrder(self,cs,order):
     '''
     SPIx.setBitOrder(chip_select,order)
-    Sets the BitOrder of the SPI device specified by chip_select to LSB First or MSB First.
+    Sets the BitOrder of the SPI device specified 
+         by chip_select to LSB First or MSB First.
     order must be either LSBFIRST or MSBFIRST
     Returns the mode of SPI device.    
     '''
-    assert self._running == True, "Must call SPIx.begin() before using setBitOrder()"
+    assert self._running == True, \
+    "Must call SPIx.begin() before using setBitOrder()"
     assert 0 <= cs < 2, "Chip Select must be 0 or 1"
     assert lsb == -1 | lsb ==1 , "either LSBFIRST or MSBFIRST"
     if order == LSBFIRST: 
@@ -154,12 +170,14 @@ class SPI_Bus(object):
   def getBitOrder(self,cs):
     '''
     SPIx.getBitOrder(chip_select)
-    Gets the Bit Order of the SPI device specified by chip_select to LSB First or MSB First.
+    Gets the Bit Order of the SPI device specified by 
+                chip_select to LSB First or MSB First.
     for LSB first : 1
     for MSB first : 0  
     Returns the Bit Order of SPI device.   
     '''
-    assert self._running == True, "Must call SPIx.begin() before using getBitOrder()"
+    assert self._running == True, \
+    "Must call SPIx.begin() before using getBitOrder()"
     assert 0 <= cs < 2, "Chip Select must be 0 or 1"
     if cs == 0:
       return self.spidev_cs0.lsbfirst
@@ -169,10 +187,12 @@ class SPI_Bus(object):
   def setMaxFreq(self,cs,freq):
     '''
     SPIx.setMaxFreq(chip_select,freq)
-    Sets the maximum frequency of the SPI device specified by chip_select to freq in HZ.
+    Sets the maximum frequency of the SPI device 
+    specified by chip_select to freq in HZ.
     frequency has to be specified in Hz.
     '''
-    assert self._running == True, "Must call SPIx.begin() before using setMaxFreq()"
+    assert self._running == True,\
+         "Must call SPIx.begin() before using setMaxFreq()"
     assert 0 <= cs < 2, "Chip Select must be 0 or 1"
     if cs == 0:
       self.spidev_cs0.msh=freq
@@ -186,7 +206,8 @@ class SPI_Bus(object):
     maximum speed in Hz
     Returns the maximum frequency in Hz.
     '''
-    assert self._running == True, "Must call SPIx.begin() before using getMaxFreq()"
+    assert self._running == True,\
+          "Must call SPIx.begin() before using getMaxFreq()"
     assert 0 <= cs < 2, "Chip Select must be 0 or 1"
     if cs == 0:
       return self.spidev_cs0.msh
@@ -196,9 +217,9 @@ class SPI_Bus(object):
   def setCShigh(self,cs):
     '''
     SPIx.setCShigh(chip_select)
-    Makes Chip Select pin of the SPI device specified by chip_select active High
+    Makes Chip Select pin of the SPI device given by chip_select active High
     '''
-    assert self._running == True, "Must call SPIx.begin() before using CShigh()"
+    assert self._running == True,"Must call SPIx.begin() before using CShigh()"
     assert 0 <= cs < 2, "Chip Select must be 0 or 1"
     if cs == 0:
       self.spidev_cs0.cshigh = True
@@ -208,7 +229,7 @@ class SPI_Bus(object):
   def setCSlow(self,cs):
     '''
     SPIx.setCSlow(chip_select)
-    Makes Chip Select pin of the SPI device specified by chip_select active Low
+    Makes Chip Select pin of the SPI device given by chip_select Active Low
     '''
     assert self._running == True, "Must call SPIx.begin() before using CSlow()"
     assert 0 <= cs < 2, "Chip Select must be 0 or 1"
