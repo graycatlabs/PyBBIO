b7a1edad215a3e39068d9f354f12a0d3e7fa6bbf
rseetham <rekha.kmit@gmail.com>
2014-07-01 16:26:20 +0000

    added functions for setting up the interrupts

--- libraries/ADT7310/adt7310.py
+++ libraries/ADT7310/adt7310.py
@@ -14,6 +14,10 @@ class ADT7310(object):
     self.spidev.setDataMode(self.cs,3)
     
   def getTemp(self):
+    '''
+    getTemp()
+    Reads the temperature value
+    '''
     self.spidev.write(self.cs,0x54)
     _t = self.spidev.read(self.cs,2)
     if ( _t[0] & 128 == 0):
@@ -21,5 +25,62 @@ class ADT7310(object):
     else:
       temp = ((((_t[0]<<8)+_t[1])>>3)-4096)/16
     return temp
+
+  def convertTemp(self,temp):
+    '''
+    convertTemp(temp)
+    converts the temp to a 2 byte value msb first
+    '''
+    t = list()
+    t.insert(0,temp>>8)
+    t.insert(1,temp & 0xff)
+    return t    
+        
+  def setLowTemp(self,temp):
+    '''
+    setLowTemp(temp)
+    Sets the Low Temperature below which the Interrupt pin will activate
+    '''
+    t = self.convertTemp(temp)
+    self.spidev.write(self.cs,[0x30,t[0],t[1]])
+    
+  def setHighTemp(self,temp):
+    '''
+    setHighTemp(temp)
+    Sets the High Temperature above which the Interrupt pin will activate.
+    '''
+    t = self.convertTemp(temp)
+    self.spidev.write(self.cs,[0x38,t[0],t[1]])
+    
+  def setCriticalTemp(self,temp):
+    '''
+    setCriticalTemp(temp)
+    Sets the Critical Temperature below which the CT pin will activate.
+    '''
+    t = self.convertTemp(temp)
+    self.spidev.write(self.cs,[0x20,t[0],t[1]])
+    
+  def setAlarm(self, int_pin , callback):
+    '''
+    setAlarm(int_pin , callback)
+    Sets the int_pin to an interrupt pin ot CT pin and calls callback() 
+        when interrupt or critical over temperature event occurs as required.
+    '''
+    self.removeAlarm(int_pin)
+    self.spidev.write(self.cs,[0x08,0x00])
+    pinMode(int_pin, INPUT, PULLUP)
+    attachInterrupt( int_pin , callback, FALLING)
+    
+  def removeAlarm(self,int_pin):
+    '''
+    removeAlarm(int_pin )
+    Removes interrupt on int_pin.
+    '''
+    detachInterrupt(int_pin)
+    
+   #Interrupt active low adn; set up critical like this? 
+    
+  
+    
     
     
