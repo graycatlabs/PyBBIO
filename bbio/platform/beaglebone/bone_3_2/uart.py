354a40d3dd5f59da4de6adcab10d1f60e56f7715
Alexander Hiam <hiamalexander@gmail.com>
2014-07-09 18:26:10 +0000

    fixed typo

--- bbio/platform/beaglebone/bone_3_2/uart.py
+++ bbio/platform/beaglebone/bone_3_2/uart.py
@@ -13,12 +13,12 @@ import pinmux
 def uartInit(uart):
   """ Muxes given serial port's header pins for use. Returns True
       if successful, False otherwise. """
-  tx_pinmux_filename = UART[self.config][1]
-  tx_pinmux_mode     = UART[self.config][2] | CONF_UART_TX
+  tx_pinmux_filename = UART[uart.config][1]
+  tx_pinmux_mode     = UART[uart.config][2] | CONF_UART_TX
   pinmux.pinMux(tx_pinmux_filename, tx_pinmux_mode)
 
-  rx_pinmux_filename = UART[self.config][3]
-  rx_pinmux_mode     = UART[self.config][4] | CONF_UART_RX
+  rx_pinmux_filename = UART[uart.config][3]
+  rx_pinmux_mode     = UART[uart.config][4] | CONF_UART_RX
   pinmux.pinMux(rx_pinmux_filename, rx_pinmux_mode)    
   # Not catching errors for now, not sure what could go wrong here...
   return True
