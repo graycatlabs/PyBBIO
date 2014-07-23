9d2e98fa92acc54d50d0eba42a436157b82a659a
rseetham <rekha.kmit@gmail.com>
2014-07-23 20:30:28 +0000

    added stopRecording() and removedthe while true

--- libraries/WebCam/webcam.py
+++ libraries/WebCam/webcam.py
@@ -3,8 +3,8 @@ import gst,sys,cv
 class WebCam(object):
 
   def __init__(self):
-    self.pipeline = gst.Pipeline("test-pipeline")
-    pass
+    self.spipeline = gst.Pipeline("test-pipeline")
+    self.rpipeline = gst.Pipeline("test-pipeline")
 
   def startStreaming(self):
     # Create the elements
@@ -18,15 +18,13 @@ class WebCam(object):
     sink = gst.element_factory_make("tcpserversink", "sink")
 
  
-    # Create the empty pipeline
-    self.pipeline = gst.Pipeline("test-pipeline")
  
-    if not source or not capsfilter or not jdecoder or not theoraenc or not video_queue or not muxogg or not sink or not self.pipeline:
+    if not source or not capsfilter or not jdecoder or not theoraenc or not video_queue or not muxogg or not sink or not self.spipeline:
       print >> sys.stderr, "Not all elements could be created."
       exit(-1)
      
     # Build the pipeline
-    self.pipeline.add(source, capsfilter, jdecoder, theoraenc, video_queue, muxogg, sink)
+    self.spipeline.add(source, capsfilter, jdecoder, theoraenc, video_queue, muxogg, sink)
     if not gst.element_link_many(source, capsfilter,  jdecoder, theoraenc, video_queue, muxogg, sink):
       print >> sys.stderr, "Elements could not be linked."
       exit(-1)
@@ -36,47 +34,27 @@ class WebCam(object):
     sink.set_property("host","127.0.0.1")
     sink.set_property("port",5000)
 
-    ret = self.pipeline.set_state(gst.STATE_PLAYING)
+    ret = self.spipeline.set_state(gst.STATE_PLAYING)
     if ret ==  gst.STATE_CHANGE_FAILURE:
       print >> sys.stderr, "Unable to set the pipeline to the playing state."
       exit(-1)
+    else:
+      return True
 
-    # Wait until error or EOS
-    bus = self.pipeline.get_bus()
-
-    # Parse message
-    while True:
-      message = bus.timed_pop_filtered(gst.CLOCK_TIME_NONE, gst.MESSAGE_STATE_CHANGED | gst.MESSAGE_ERROR | gst.MESSAGE_EOS)
-      if message.type == gst.MESSAGE_ERROR:
-         err, debug = message.parse_error()
-         print >> sys.stderr, "Error received from element %s: %s"% (message.src.get_name(), err)
-         print >> sys.stderr, "Debugging information: %s"% debug
-         break
-      elif message.type == gst.MESSAGE_EOS:
-         print "End-Of-Stream reached."
-         break
-      elif message.type == gst.MESSAGE_STATE_CHANGED:
-         if isinstance(message.src, gst.Pipeline):
-             old_state, new_state, pending_state = message.parse_state_changed()
-             print ("Pipeline state changed from %s to %s."% 
-                    (gst.element_state_get_name(old_state), gst.element_state_get_name (new_state)))
-      else:
-         print >> sys.stderr, "Unexpected message received."
-
-    self.pipeline.set_state(gst.STATE_NULL)
-    
   
+  def stopStreaming(self):
+    self.spipeline.set_state(gst.STATE_NULL)
     
-  def stopPipeline(self):
-    self.pipeline.set_state(gst.STATE_STOPPING)
+  def stopRecording(self):
+    self.rpipeline.set_state(gst.STATE_NULL)
     
-  def capturePic(self,filename)
+  def captureSnapshot(self,filename):
     filename = str(filename)
     capture = cv.CaptureFromCAM(-1) #-1 will select the first camera available, usually /dev/video0 on linux
     cv.SetCaptureProperty(capture, cv.CV_CAP_PROP_FRAME_WIDTH, 320)
     cv.SetCaptureProperty(capture, cv.CV_CAP_PROP_FRAME_HEIGHT, 240)
     im = cv.QueryFrame(capture)
-    cv.SaveImage(filename+".jpg")
+    cv.SaveImage(filename+".jpg",im)
     
   def startRecording(self,filename):
     filename = str(filename)
@@ -90,16 +68,12 @@ class WebCam(object):
     muxogg= gst.element_factory_make("oggmux", "muxogg")
     sink = gst.element_factory_make("filesink", "sink")
 
- 
-    # Create the empty pipeline
-    self.pipeline = gst.Pipeline("test-pipeline")
- 
-    if not source or not capsfilter or not jdecoder or not theoraenc or not video_queue or not muxogg or not sink or not self.pipeline:
+    if not source or not capsfilter or not jdecoder or not theoraenc or not video_queue or not muxogg or not sink or not self.rpipeline:
       print >> sys.stderr, "Not all elements could be created."
       exit(-1)
      
     # Build the pipeline
-    self.pipeline.add(source, capsfilter, jdecoder, theoraenc, video_queue, muxogg, sink)
+    self.rpipeline.add(source, capsfilter, jdecoder, theoraenc, video_queue, muxogg, sink)
     if not gst.element_link_many(source, capsfilter,  jdecoder, theoraenc, video_queue, muxogg, sink):
       print >> sys.stderr, "Elements could not be linked."
       exit(-1)
@@ -108,32 +82,10 @@ class WebCam(object):
     capsfilter.set_property("caps", caps)
     sink.set_property("location",filename+".ogg")
 
-    ret = self.pipeline.set_state(gst.STATE_PLAYING)
+    ret = self.rpipeline.set_state(gst.STATE_PLAYING)
     if ret ==  gst.STATE_CHANGE_FAILURE:
       print >> sys.stderr, "Unable to set the pipeline to the playing state."
       exit(-1)
-
-    # Wait until error or EOS
-    bus = self.pipeline.get_bus()
-
-    # Parse message
-    while True:
-      message = bus.timed_pop_filtered(gst.CLOCK_TIME_NONE, gst.MESSAGE_STATE_CHANGED | gst.MESSAGE_ERROR | gst.MESSAGE_EOS)
-      if message.type == gst.MESSAGE_ERROR:
-         err, debug = message.parse_error()
-         print >> sys.stderr, "Error received from element %s: %s"% (message.src.get_name(), err)
-         print >> sys.stderr, "Debugging information: %s"% debug
-         break
-      elif message.type == gst.MESSAGE_EOS:
-         print "End-Of-Stream reached."
-         break
-      elif message.type == gst.MESSAGE_STATE_CHANGED:
-         if isinstance(message.src, gst.Pipeline):
-             old_state, new_state, pending_state = message.parse_state_changed()
-             print ("Pipeline state changed from %s to %s."% 
-                    (gst.element_state_get_name(old_state), gst.element_state_get_name (new_state)))
-      else:
-         print >> sys.stderr, "Unexpected message received."
-
-    self.pipeline.set_state(gst.STATE_NULL)   
+    else:
+      return True
     
