fe618708e85c38210d834121a79ff4b8ee19bf6d
rseetham <rekha.kmit@gmail.com>
2014-07-23 10:48:16 +0000

    example for accelerometer

---
+++ examples/mma7660-test.py
@@ -0,0 +1,16 @@
+from bbio import *
+from MMA7660 import *
+
+acc = MMA7660(2)
+
+def setup():
+  pass
+  
+def loop():
+  x = acc.getX()
+  y = acc.getY()
+  z = acc.getZ()
+  print "X : "+str(x)+" Y : "+str(y)+" Z : "+str(z)
+  delay(1000)
+  
+run(setup,loop)
