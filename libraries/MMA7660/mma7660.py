be261c1cd883dff122db5182fa6b54c6cdbc9741
rseetham <rekha.kmit@gmail.com>
2014-07-24 16:19:44 +0000

    changed init() to setup tap, shake and oreintation and wrote function for setting up interrupts

--- libraries/MMA7660/mma7660.py
+++ libraries/MMA7660/mma7660.py
@@ -20,7 +20,7 @@ class MMA7660(object):
   MODE_ACTIVE = 0x01
   SRATE_16 = 0x03
   SRATE_120 = 0x00
-  SR_FLIT = 2<<6
+  SR_FLIT = 2<<5
   INT_TAP = 1<<2
   PD_TAP_THRESH = 0x14
   PDET_TAP_X = 0<<5
@@ -38,12 +38,12 @@ class MMA7660(object):
     self.int_pin = None 
     self.i2cdev.begin()
     self.i2cdev.write(self.MMA7660_ADDR,self.REG_MODE,self.MODE_STAND_BY)
-    self.i2cdev.write(self.MMA7660_ADDR,self.REG_SR,self.SRATE_120)
+    self.i2cdev.write(self.MMA7660_ADDR,self.REG_SR,self.SRATE_120 | self.SR_FLIT)
     self.i2cdev.write(self.MMA7660_ADDR,self.REG_PD,self.PD_TAP_THRESH)
     pdet = self.PDET_TH | self.PDET_TAP_X | self.PDET_TAP_Y | self.PDET_TAP_Z
     self.i2cdev.write(self.MMA7660_ADDR,self.REG_PDET,pdet)
     self.i2cdev.write(self.MMA7660_ADDR,self.REG_MODE,self.MODE_ACTIVE)
-    self.tilt_val = None
+    self.int_pin = None
     
   def getX(self):
     x = self.i2cdev.read(self.MMA7660_ADDR,self.REG_X)
@@ -68,25 +68,13 @@ class MMA7660(object):
     
 
 
-  def setTap(self,tap_count = 7):
-    assert 0<tap_count<256 , "tap_count must be between 1 and 255"
-    TAP_THRESH = 0x14
-    PDET_TAP_X = 0<<5
-    PDET_TAP_Y = 0<<6
-    PDET_TAP_Z = 1<<7
+  def setTapAlarm(self,int_pin,callabck):
+    self._setAlarm(int_pin,callabck) 
     self.i2cdev.write(self.MMA7660_ADDR,self.REG_MODE,self.MODE_STAND_BY)
-    self.i2cdev.write(self.MMA7660_ADDR,self.REG_SR,self.SRATE_120)
     self.i2cdev.write(self.MMA7660_ADDR,self.REG_INTSU,self.INT_TAP)
-    pdet = 6 | TAP_X | TAP_Y | TAP_Z
-    self.i2cdev.write(self.MMA7660_ADDR,self.REG_PD,self.TAP_THRESH)
-    self.i2cdev.write(self.MMA7660_ADDR,self.REG_PDET,pdet)
     self.i2cdev.write(self.MMA7660_ADDR,self.REG_MODE,self.MODE_ACTIVE)
     
-  def _readTilt(self):
-    self.tilt_val = self.i2cdev.read(self.MMA7660_ADDR,self.REG_TILT)
-    while((tilt_val>>6)>=1):
-      self.tilt_val = self.i2cdev.read(self.MMA7660_ADDR,self.REG_TILT)
-
+ 
   def detectTap(self):
     tap = self.i2cdev.read(self.MMA7660_ADDR,self.REG_TILT)
     print "TAP I: "+str(tap)
@@ -95,13 +83,7 @@ class MMA7660(object):
     tap = (tap&0x20)>>5
     return tap
     
-  def setOrientation(self,orient_debounce=1):
-    assert 0<orient_debounce<8 , "orient_debounce must be between 1 and 8"
-    flit = (orient_debounce+1)<<6 | self.SRATE_120
-    self.i2cdev.write(self.MMA7660_ADDR,self.REG_MODE,self.MODE_STAND_BY)
-    self.i2cdev.write(self.MMA7660_ADDR,self.REG_SR,flit)
-    self.i2cdev.write(self.MMA7660_ADDR,self.REG_MODE,self.MODE_ACTIVE)
-    
+
   def getOrientation(self):
     orient = self.i2cdev.read(self.MMA7660_ADDR,self.REG_TILT)
     while((orient&0x40)>>6==1):
@@ -121,7 +103,8 @@ class MMA7660(object):
     elif bafro == 2:
       print "Orientation : Back: Equipment is lying on its back"
    
-  def setShake(self):
+  def setShakeAlarm(self,int_pin,callabck):
+    self._setAlarm(int_pin,callabck) 
     SHAKE_X = 1<<7
     SHAKE_Y = 1<<6
     SHAKE_Z = 1<<5
@@ -139,11 +122,15 @@ class MMA7660(object):
     shake = (shake&0x80)>>7
     return shake
     
-  def setAlarm(self,int_pin):
-    #self.removeAlarm()
+  def _setAlarm(self,int_pin,callback):
+    self._removeAlarm()
     self.int_pin = int_pin
     pinMode(self.int_pin, INPUT, PULLUP)
-    attachInterrupt(self.int_pin,callback,FALLING)    
+    attachInterrupt(self.int_pin,callback,FALLING) 
+    
+  def _removeAlarm(self):
+    if self.int_pin:
+      detachInterrupt(self.int_pin)
     
     
     
