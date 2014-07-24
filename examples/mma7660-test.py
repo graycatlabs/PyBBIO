9253efe23ba04a44cef716f38a290c0e0cef70da
rseetham <rekha.kmit@gmail.com>
2014-07-24 16:18:12 +0000

    testing

--- examples/mma7660-test.py
+++ examples/mma7660-test.py
@@ -4,16 +4,22 @@ from MMA7660 import *
 acc = MMA7660(2)
 
 def setup():
-  acc.setOrientation()
+  #acc.setOrientation()
+  #acc.setTap()
+  #acc.setShake()
+  pass
   
 def loop():
   x = acc.getX()
   y = acc.getY()
   z = acc.getZ()
   print "X : "+str(x)+" Y : "+str(y)+" Z : "+str(z)
-  #tap = acc.detectTap()
-  #print "Tap : "+str(tap)
-  print acc.getOrientation()
+  
+  tap = acc.detectTap()
+  print "Tap : "+str(tap)
+  acc.getOrientation()
+  shake = acc.getShake()
+  print "Shake: "+str(shake) 
   delay(1000)
   
 run(setup,loop)
