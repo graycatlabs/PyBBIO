031d2a6ec8fb31bd661040751dfe42841abd1c5e
rseetham <rekha.kmit@gmail.com>
2014-08-05 02:10:49 +0000

    testing

--- examples/webcam_test.py
+++ examples/webcam_test.py
@@ -1,10 +1,19 @@
 from bbio import *
-from WebCam import *
+from WebCam import WebCam
 
 cam = WebCam()
+
 cam.startStreaming()
-#cam.startRecording("sample")
+cam.startRecording("sample1")
+cam.takeSnapshot("sam1")
 delay(30000)
 cam.stopStreaming()
-#cam.stopRecording()
-#cam.captureSnapshot("sam")
+cam.stopRecording()
+cam.takeSnapshot("sam2")
+delay(10000)
+cam.startStreaming()
+cam.startRecording("sample2")
+delay(30000)
+cam.stopStreaming()
+cam.stopRecording()
+cam.takeSnapshot("sam3")
