e748dc0d19e47e1ffbc81796047b51a1e69ce8b3
rseetham <rekha.kmit@gmail.com>
2014-08-12 17:50:41 +0000

    removed recording

--- libraries/WebCam/webcam.py
+++ libraries/WebCam/webcam.py
@@ -1,16 +1,6 @@
-'''
-WebCam - v0.1
-Copyright 2014 Rekha Seethamraju
-
-Library for streaming video and taking a picture from a webcam connected to  
-the beaglebone black.
-
-WebCam is released as part of PyBBIO under its MIT license.
-See PyBBIO/LICENSE.txt
-'''
-
-from bbio import delay, addToCleanup
+from bbio import *
 import gst,sys, gobject, pygtk, gst.video
+
 class WebCam(object):
 
   def __init__(self,video_device=0):
@@ -26,67 +16,48 @@ class WebCam(object):
     self.srcbin = self._sourcebin()
     self.videocbin = self._videoconvertbin()
     
-    self.streaming = 0
-    self.recording = 0
-    
     self.fakesink = gst.element_factory_make("fakesink")
     self.fakestreamsink = gst.element_factory_make("fakesink")
-    self.fakerecordsink = gst.element_factory_make("fakesink")
     self.streamsink = gst.element_factory_make("tcpserversink")
-    self.recordsink = gst.element_factory_make("filesink","record_sink")
+    self.bufferqueue = gst.element_factory_make("queue")
     
     self.pic_tee = gst.element_factory_make("tee")
     self.pic_queue = gst.element_factory_make("queue")
     self.video_queue = gst.element_factory_make("queue")
-
-    self.video_tee = gst.element_factory_make("tee")
-    self.stream_queue = gst.element_factory_make("queue")
-    self.record_queue = gst.element_factory_make("queue")
+    
+    self.streaming = 0
     
     if not (self.srcbin and self.videocbin and self.pic_tee and self.pic_queue\
              and self.video_queue and self.fakesink and self.fakestreamsink and \
-             self.video_tee and self.stream_queue and self.record_queue and \
-             self.fakerecordsink and self.streamsink):
-      raise Exception('Not all elements could be created.')
-    
+             self.pipeline and self.bufferqueue):
+      raise Exception ("Elements could not be created")
+      
     self.pipeline.add(self.srcbin,self.videocbin,self.pic_tee,self.pic_queue,\
-                 self.video_queue,self.fakesink,self.fakestreamsink,self.video_tee,\
-                 self.stream_queue,self.record_queue,self.fakerecordsink)
-                 #self.recordsink,self.streamsink)
-    
+                 self.video_queue,self.fakesink,self.fakestreamsink, \
+                 self.bufferqueue)
     if (not gst.element_link_many(self.srcbin,self.pic_tee) or \
         not gst.element_link_many(self.pic_queue,self.fakesink) or \
-        not gst.element_link_many(self.video_queue,self.videocbin,self.video_tee) or \
-        not gst.element_link_many(self.stream_queue,self.fakestreamsink) or \
-        not gst.element_link_many(self.record_queue,self.fakerecordsink)):
+        not gst.element_link_many(self.video_queue,self.videocbin,self.bufferqueue,\
+                                  self.fakestreamsink)):
       raise Exception("Elements could not be linked")
       
     self.tee_pic_pad = self.pic_tee.get_request_pad("src%d")
     self.tee_video_pad = self.pic_tee.get_request_pad("src%d")
     self.queue_pic_pad = self.pic_queue.get_static_pad("sink")
     self.queue_video_pad = self.video_queue.get_static_pad("sink")
-
-    self.tee_stream_pad = self.video_tee.get_request_pad("src%d")
-    self.tee_record_pad = self.video_tee.get_request_pad("src%d")
-    self.queue_stream_pad = self.stream_queue.get_static_pad("sink")
-    self.queue_record_pad = self.record_queue.get_static_pad("sink")
-
-    self.tee_pic_pad.link(self.queue_pic_pad) 
-    self.tee_video_pad.link(self.queue_video_pad) 
-
-    self.tee_stream_pad.link(self.queue_stream_pad) 
-    self.tee_record_pad.link(self.queue_record_pad)
     
-    self.src_stream_pad = self.stream_queue.get_pad("src")
-    self.src_record_pad = self.record_queue.get_pad("src")
+    if (self.tee_pic_pad.link(self.queue_pic_pad) != gst.PAD_LINK_OK or
+        self.tee_video_pad.link(self.queue_video_pad) != gst.PAD_LINK_OK):
+      raise Exception ("Tee and queues could not be linked")
     
+    self.src_stream_pad = self.bufferqueue.get_pad("src")
+    self.sink_stream_pad = self.bufferqueue.get_pad("sink")
     self.fake_stream_pad = self.fakestreamsink.get_pad("sink")
-    self.fake_record_pad = self.fakerecordsink.get_pad("sink")
     self.stream_sink_pad = self.streamsink.get_pad("sink")
-    self.record_sink_pad = self.recordsink.get_pad("sink")
     
-    if not (self.src_stream_pad and self.src_record_pad and self.fake_stream_pad and self.fake_record_pad and self.stream_sink_pad):
-      raise Exception("this =(")  
+    if not (self.src_stream_pad and self.fake_stream_pad and self.stream_sink_pad):
+      raise Exception("Pads could not be created") 
+      
     ret = self.pipeline.set_state(gst.STATE_PLAYING)
     if ret ==  gst.STATE_CHANGE_FAILURE:
       raise Exception("Unable to set the pipeline to the playing state.")
@@ -94,19 +65,13 @@ class WebCam(object):
     self.streamsink.set_state(gst.STATE_NULL)
     delay(10000)
     addToCleanup(self.stopPipeline)
-    
+
   def _sourcebin(self):
-    '''
-    _sourcebin()
-    Makes a bin : v4l2src ! image/jpeg,width=640,height=480,framerate=30/1 ! \
-                  jpegdec
-    '''
     bin = gst.Bin("srcbin")
     source = gst.element_factory_make("v4l2src")
     caps = gst.Caps("image/jpeg,width=640,height=480,framerate=30/1")
     capsfilter = gst.element_factory_make("capsfilter")
     jdecoder = gst.element_factory_make("jpegdec")
-    source.set_property("device",self.video_device)
     bin.add(source,capsfilter,jdecoder)
     gst.element_link_many(source,capsfilter,jdecoder)
     # ghostpad 
@@ -117,11 +82,6 @@ class WebCam(object):
     return bin
     
   def _videoconvertbin(self):
-    '''
-    _videoconvertbin()
-    Makes a bin with :  
-    theoraenc ! queue ! oggmux
-    '''
     bin = gst.Bin("vconvertbin")
     theoraenc = gst.element_factory_make("theoraenc")
     buffer_queue = gst.element_factory_make("queue")
@@ -140,38 +100,6 @@ class WebCam(object):
     bin.add_pad(sink_ghost_pad)
     return bin
     
-      
-  def startStreaming(self,port = 5000):
-    '''
-    startStreaming((optional)port)
-    starts streaming video from the webcam to http://your_bbb's_ip_sddress:port
-    port defaults to 5000
-    NOTE : Once port no. is set trying to change it will show an error.
-    '''
-    self.streamsink.set_property("host","127.0.0.1")
-    self.streamsink.set_property("port",int(port))
-    self.streamsink.set_state(gst.STATE_PLAYING)
-    if self.streaming == 0:
-      self.pipeline.add(self.streamsink)
-      self.streaming = 1
-    self.queue_stream_pad.set_blocked(True)
-    self.fakestreamsink.set_state(gst.STATE_NULL)
-    self.src_stream_pad.unlink(self.fake_stream_pad)
-    self.src_stream_pad.link(self.stream_sink_pad)
-    self.queue_stream_pad.set_blocked(False)
-   
-  def stopStreaming(self):
-    '''
-    stopStreaming()
-    stops the streaming
-    '''
-    self.fakestreamsink.set_state(gst.STATE_PLAYING)
-    self.queue_stream_pad.set_blocked(True)
-    self.streamsink.set_state(gst.STATE_NULL)
-    self.src_stream_pad.unlink(self.stream_sink_pad)
-    self.src_stream_pad.link(self.fake_stream_pad)
-    self.queue_stream_pad.set_blocked(False)
-  
   def takeSnapshot(self,filename):
     '''
     takeSnapshot(filename)
@@ -187,7 +115,39 @@ class WebCam(object):
       fh.write(str(buf))
       print "took a picture"
     self.fakesink.set_state(gst.STATE_PLAYING)
-  
+    
+  def startStreaming(self,port = 5000):
+    '''
+    startStreaming((optional)port)
+    starts streaming video from the webcam to http://your_bbb's_ip_sddress:port
+    port defaults to 5000
+    NOTE : Once port no. is set trying to change it will show an error.
+    '''
+    self.streamsink.set_property("host","127.0.0.1")
+    self.streamsink.set_property("port",int(port))
+    self.streamsink.set_state(gst.STATE_PLAYING)
+    if self.streaming == 0:
+      self.pipeline.add(self.streamsink)
+      self.streaming = 1
+    self.sink_stream_pad.set_blocked(True)
+    self.fakestreamsink.set_state(gst.STATE_NULL)
+    self.src_stream_pad.unlink(self.fake_stream_pad)
+    self.src_stream_pad.link(self.stream_sink_pad)
+    self.sink_stream_pad.set_blocked(False)
+   
+  def stopStreaming(self):
+    '''
+    stopStreaming()
+    stops the streaming
+    '''
+    self.fakestreamsink.set_state(gst.STATE_PLAYING)
+    self.sink_stream_pad.set_blocked(True)
+    self.streamsink.set_state(gst.STATE_NULL)
+    self.src_stream_pad.unlink(self.stream_sink_pad)
+    self.src_stream_pad.link(self.fake_stream_pad)
+    self.sink_stream_pad.set_blocked(False)
+    
+    
   def stopPipeline(self):
     '''
     stopPipeline()
