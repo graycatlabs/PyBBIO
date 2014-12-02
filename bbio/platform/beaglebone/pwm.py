6fb0740888858757a40a7f5b0678fccefbc6d175
Alexander Hiam <hiamalexander@gmail.com>
2014-12-02 03:05:59 +0000

    fixed import

--- bbio/platform/beaglebone/pwm.py
+++ bbio/platform/beaglebone/pwm.py
@@ -5,7 +5,7 @@
 # 
 # Beaglebone PWM driver for kernel >= 3.8
 
-from bbio.platform.beaglebone.bone_3_8 import cape_manager
+from bbio.platform.beaglebone import cape_manager
 from bbio.platform.beaglebone import sysfs
 from bbio.util import delay, addToCleanup
 from config import RES_8BIT, PWM_PINS, PWM_PERIOD, PWM_DUTY, PWM_POLARITY,\
