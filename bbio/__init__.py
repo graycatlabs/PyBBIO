bfac1ef7f83e592fb69cfdf64490ec1e0cad6c6d
Alexander Hiam <hiamalexander@gmail.com>
2014-12-05 22:16:54 +0000

    better importing of libraries

--- bbio/__init__.py
+++ bbio/__init__.py
@@ -3,10 +3,14 @@
 
 try:
   from .bbio import run, stop
-except ImportError:
-  # run() and stop() not defined in interactive prompt
-  pass
-
+except ImportError, e:
+  if e.args[0] == 'cannot import name run':
+    # run() and stop() not defined in interactive mode
+    pass
+  else: raise
+  
 from .common import addToCleanup, millis, micros, delay, delayMicroseconds
 
 from .platform import *
+
+import libraries
--- bbio/libraries/__init__.py
+++ bbio/libraries/__init__.py
@@ -1,16 +1,2 @@
 # bbio/libraries/__init__.py
 
-__all__ = [
-  'ADS786x',
-  'ADT7310',
-  'BBIOServer',
-  'DACx311',
-  'EventIO',
-  'MAX31855',
-  'MMA7660',
-  'PhantStream',
-  'RotaryEncoder',
-  'SafeProcess',
-  'Servo',
-  'WebCam',
-]
