f917335c84c37007ae812905596b0e28ad2467a3
Alexander Hiam <alex@graycat.io>
2015-04-17 00:10:05 -0400

    oops, I broke it. Fixed now

--- bbio/platform/beaglebone/spi.py
+++ bbio/platform/beaglebone/spi.py
@@ -3,16 +3,16 @@
 # github.com/graycatlabs/PyBBIO
 # MIT License
 
-import cape_manager
+import cape_manager, bbio
 from bbio.platform.util import spidev
 
 class SPIBus(spidev.SPIDev):
   def open(self):
     overlay = 'BB-SPIDEV%i' % (self.bus-1)
     cape_manager.load(overlay, auto_unload=False)
-    bbio.delay(250) # Give driver time to load
+    bbio.common.delay(250) # Give driver time to load
     assert cape_manager.isLoaded(overlay), "Could not load SPI overlay"
-    super(SPI_Bus, self).open()
+    super(SPIBus, self).open()
     # Initialize to default parameters:
     self.setCSActiveLow(0)
     self.setBitsPerWord(0, 8)
