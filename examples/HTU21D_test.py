9e247ef9096fb708ab0f051cffdfc96c35d6b694
Alexander Hiam <alex@graycat.io>
2015-04-17 13:05:42 -0400

    changed Wire2 to I2C2

--- examples/HTU21D_test.py
+++ examples/HTU21D_test.py
@@ -12,8 +12,8 @@ from bbio import *
 # Import the HTU21D class from the HTU21D library:
 from bbio.libraries.HTU21D import HTU21D
 
-# Create a new instance of the HTU21D class using the Wire2 I2C bus
-htu = HTU21D(Wire2)
+# Create a new instance of the HTU21D class using the I2C2 I2C bus
+htu = HTU21D(I2C2)
 
 def setup():
   # The HTU21D is initialized when the HTU21D class is instantiated,
