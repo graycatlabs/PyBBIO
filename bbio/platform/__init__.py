4145f9434e1b36bca91f034b9dd2ba2c331f6709
Alexander Hiam <alex@graycat.io>
2015-09-15 23:00:52 -0400

    raise error if platform not supported

--- bbio/platform/__init__.py
+++ bbio/platform/__init__.py
@@ -1,7 +1,17 @@
 
 from platform import detect_platform
 _platform = detect_platform()
+
 if "BeagleBone" in _platform:
   from beaglebone import *
   import beaglebone
+
+#elif "SomeOtherPlatform" in _platform:
+#  ...
+
+else:
+  # Unsupported platform
+  raise ImportError("Cannot import PyBBIO, platform not supported")
+
+# No need to keep this in the namespace:
 del _platform
